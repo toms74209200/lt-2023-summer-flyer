09aabd52b85a45402a1cab82dcb43c2a4b1af5df
toms74209200 <motomotomato@gmail.com>
2023-06-07 00:41:19 +0900

    :sparkles: initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const markdown_file = (() => {
+    const url = location.href;
+    if (url.includes('html')) {
+      return location.origin + '/docs/main.md';
+    }
+    return url + '/docs/main.md';
+  })();
+  
+  const main = () => {
+    fetch(markdown_file)
+      .then(res => {
+        return res.text();
+      }).then(data => {
+        markdown.innerHTML = marked.parse(data);
+      });
+  }
+  
+  main();
